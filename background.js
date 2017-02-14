ecf514c9ec9898189545c7ec552c395f9a59b767
st4rgut22 <edward.arbor@gmail.com>
2017-02-13 16:23:25 -0800

    Update background.js
    see second commit for content js

--- background.js
+++ background.js
@@ -4,22 +4,16 @@ chrome.browserAction.onClicked.addListener(function(tab){
 		chrome.tabs.sendMessage(activeTab.id,{"message":"browserAction"})
 	  });
 })
+
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
 	if (request.message=="linkClicked"){
 		var d = request.description
-		console.log(d)
 		if (d.indexOf('.')>=0){ //this will always evaluate to true and execute, leaving empty string ||||
 			var d = d.slice(0,d.indexOf('.')) //get a snippet of text
 		}
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	    var activeTab = tabs[0];
+	setTimeout(function(){chrome.tabs.sendMessage(activeTab.id,{"newmessage":"newKeyName","msg":d}); console.log(d); console.log(activeTab.id)},3000)
+	  });
 	}
-	setTimeout(function(){chrome.storage.local.set({"keyName": d});},2000)
 })
-/*
-chrome.runtime.onMessage(function(request,sender,sendResponse){
-	if (request.message=="linkClicked"){
-		var description = request.description
-	}
-	var description = d
-	var higlightThis = document.match("description")
-	chrome.highlight(highlightThis)
-})*/
