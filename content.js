bcbfa86b40dc448e8b8e2c9ecc0de9ed7caad386
st4rgut22 <edward.arbor@gmail.com>
2017-02-13 16:22:24 -0800

    Update content.js
    replaced chrome storage with message passing

--- content.js
+++ content.js
@@ -8,30 +8,30 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
 			if (index>=0){
 				var description = description.slice(index+2,description.length-1)
 			}
-			alert(description)
 			chrome.runtime.sendMessage({"message":"linkClicked","description":description})
 		})		
 	}
 })
-chrome.storage.onChanged.addListener(function(changes,areaName){
-alert('changed')
 
-chrome.storage.local.get("keyName",function(items) {//initialize the application
-			alert(items.keyName)
-			var p = document.getElementsByTagName('p')
-			for (n=0;n<p.length;n++){
-				console.log(p[n].textContent)
-				if (p[n].textContent.includes(items.keyName)){
-					console.log('found a match at paragraph' + p[n])
-					var newStr = p[n].textContent.replace(items.keyName,"<span style='background-color:yellow'>" + items.keyName + "</span>")
-					p[n].innerHTML=newStr
-			        $('html, body').animate({
-			        scrollTop: $('p:eq('+n+')').offset().top + 'px'
-			        }, 'fast')			
-					return
-				}
-				else{console.log('no match found')}
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+		if (request.newmessage=="newKeyName"){
+					console.log(request.msg)
+					var p = document.getElementsByTagName('p')
+					for (n=0;n<p.length;n++){
+						console.log(p[n].textContent)
+						if (p[n].textContent.includes(request.msg)){
+							console.log('found a match at paragraph' + p[n])
+							var newStr = p[n].textContent.replace(request.msg,"<span style='background-color:yellow'>" + request.msg + "</span>")
+							p[n].innerHTML=newStr
+					        $('html, body').animate({
+					        scrollTop: $('p:eq('+n+')').offset().top + 'px'
+					        }, 'fast')			
+							return
+						}
+						else{console.log('no match found')}
+					}
 			}
-
-})	
-})
+		}
+)
+//})	
+//})
